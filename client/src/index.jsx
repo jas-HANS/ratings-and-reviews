b960c09f27516b694ed863beac2af22d31b8cb97
Nick Timms <nicktimms1@hotmail.com>
2020-09-23 14:06:07 -0600

    Configuration Set Up -- Testing Framework not Installed

---
+++ client/src/index.jsx
@@ -0,0 +1,8 @@
+// Renders the react components to the html
+
+import App from './App.jsx';
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+
+ReactDOM.render(<App/>, document.getElementById('root'));
