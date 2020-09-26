24800f315b082e33e507f9910def81a1b7deb83d
Nick Timms <nicktimms1@hotmail.com>
2020-09-26 17:37:56 -0600

    Fixed a small issue with passing down review_id to the reviewHelpful component

--- client/lib/routes.js
+++ client/lib/routes.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const searchReviews = (callback) => {
-  const id = 4;
+  const id = 125;
   axios.get(`http://52.26.193.201:3000/reviews/${id}/list`)
     .then((data) => callback(null, data.data))
     .catch((err) => callback(err, null));
