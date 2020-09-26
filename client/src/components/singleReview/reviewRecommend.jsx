fff1f2bfb40a5f9927b8ef8cd07e3c0ed0d8f838
Nick Timms <nicktimms1@hotmail.com>
2020-09-26 16:15:16 -0600

    Fixed an issue where the Checkmark for recommend would show if it was not recommended

--- client/src/components/singleReview/reviewRecommend.jsx
+++ client/src/components/singleReview/reviewRecommend.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const Recommend = ({ recommend }) => (
   <div className="recommend">
     {recommend ? 'I recommend this product ' : ''}
-    <span className="recommend-checkmark">&#x2713;</span>
+    {recommend ? <span className="recommend-checkmark">&#x2713;</span> : ''}
   </div>
 );
 
