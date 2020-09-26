3eb4f26534b3e9a6a9014f8c9f11743a67687d5d
Nick Timms <nicktimms1@hotmail.com>
2020-09-26 13:06:50 -0600

    Created a subcomponent folder for a single review

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,14 +7,14 @@ import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 
 // Importing My Components
-import ReviewTile from './components/reviewTile';
+import ReviewTile from './components/singleReview/reviewTile';
 
 // Importing API Request methods
 import searchReviews from '../lib/routes';
 
 const App = () => {
   const [reviews, getReviews] = useState([]); // State of reviews for current product
-  const [loaded, isLoading] = useState(true);
+  const [exists, reviewExists] = useState(true);
 
   useEffect(() => { // Sets the initial state of reviews
     searchReviews((err, data) => {
@@ -24,9 +24,9 @@ const App = () => {
         const info = data.data.results[0]; // Reviews array at the first index.
         getReviews(info); // Set the reviews state to the data from the axios request
         if (info === undefined) { // Checking if the reviews array at the first index is undefined
-          isLoading(true);
+          reviewExists(true);
         } else {
-          isLoading(false); // Conditionally render the reviews when the data comes in
+          reviewExists(false); // Conditionally render the reviews when the data comes in
         }
       }
     });
@@ -39,11 +39,7 @@ const App = () => {
         <Col xs="12" sm="8">
           <Container className="main-container">
             <h1 className="title">Ratings and Reviews</h1>
-            {loaded || (
-            <ReviewTile
-              data={reviews}
-            />
-            )}
+            {exists || (<ReviewTile data={reviews} />)}
           </Container>
         </Col>
         <Col xs="0" sm="2" />
