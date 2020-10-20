f8cd3ab320e29a1eb0a81feaa4ab08dfb826dd58
Alison Sipos <alison.sipos@icloud.com>
2020-10-19 20:12:01 -0600

    Created Models

---
+++ database/models/ReviewModel.js
@@ -0,0 +1,72 @@
+/* eslint-disable no-console */
+// include Mongoose
+const mongoose = require('mongoose');
+
+// With Mongoose, everything is derived from a Schema. Let's get a reference to it and define
+// The following are all the valid SchemaTypes in Mongoose.
+//   String
+//   Number
+//   Date
+//   Buffer
+//   Boolean
+//   Mixed
+//   ObjectId
+//   Array
+//   Decimal128
+//   Map
+//   Schema
+
+const reviewSchema = new mongoose.Schema({
+  product_id: Number,
+  rating: Number,
+  summary: String,
+  body: String,
+  recommend: Number,
+  name: String,
+  email: String,
+  helpfulness: Number,
+  report: Boolean,
+  photos: [{
+    id: Number,
+    thumbnail_url: String,
+    url: String,
+  }],
+  characteristics: {
+    ratings: {
+      1: Number,
+      2: Number,
+      3: Number,
+      4: Number,
+      5: Number,
+    },
+    recommended: {
+      0: Number,
+      1: Number,
+      null: Number,
+    },
+    characteristics: {
+      Fit: {
+        id: Number,
+        value: Number,
+      },
+      Length: {
+        id: Number,
+        value: Number,
+      },
+      Comfort: {
+        id: Number,
+        value: Number,
+      },
+      Quality: {
+        id: Number,
+        value: Number,
+      },
+    },
+  },
+});
+// compile schema into a Model. A model is a class with which we construct documents.
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = {
+  Review,
+};
