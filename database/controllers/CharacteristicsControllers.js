72bc832c9f0742ba4fc09eaa9db7f6597a2bd9da
Alison Sipos <alison.sipos@icloud.com>
2020-10-21 11:59:08 -0600

    Created Characteristic Controller that grabs the name of a characteristic tied to a product id.

---
+++ database/controllers/CharacteristicsControllers.js
@@ -0,0 +1,17 @@
+import { Characteristics } from '../models/CharacteristicsModel';
+
+const CharacteristicsController = {
+  getCharacteristicName: (productId, callback) => {
+    Characteristics.find({ product_id: productId }, (error, response) => {
+      if (error) {
+        callback(error, null);
+      } else {
+        callback(null, response);
+      }
+    });
+  },
+};
+
+module.exports = {
+  CharacteristicsController,
+};
