bf3c664e32ad54c911209d1f0d00378e4bb797aa
Alison Sipos <alison.sipos@icloud.com>
2020-10-21 11:38:53 -0600

    Edited controllers for reviews.

--- database/controllers/ReviewControllers.js
+++ database/controllers/ReviewControllers.js
@@ -3,56 +3,102 @@ const { Review } = require('../models/ReviewModel');
 
 const ReviewController = {
   getAllReviews: (productId, callback) => {
-    Review.find({ product: productId }, (error, doc) => {
+    // const maxReviews = params.count || 5;
+    Review.find({ product: productId }, (error, response) => {
       if (error) {
         callback(error, null);
       } else {
-        callback(null, doc.results);
+        callback(null, response.results);
+      }
+    });// .setOptions({ limit: maxReviews });
+  },
+  getOneReview: (reviewId, callback) => {
+    Review.findOne({ _id: reviewId }, (error, response) => {
+      if (error) {
+        callback(error, null);
+      } else {
+        callback(null, response);
       }
     });
   },
-  getOneReview: (reviewId, callback) => {
-    Review.find({ _id: reviewId }, (error, doc) => {
+  setOneReview: (product, callback) => {
+    Review.findByIdAndUpdate({ _id: product }, (error, response) => {
       if (error) {
         callback(error, null);
       } else {
-        callback(null, doc);
+        callback(null, response);
       }
     });
   },
   getReviewPhotos: (reviewId, callback) => {
-    Review.find({ _id: reviewId }, (error, doc) => {
+    Review.find({ _id: reviewId }, (error, response) => {
       if (error) {
         callback(error, null);
       } else {
-        callback(null, doc.photos);
+        callback(null, response.photos);
       }
     });
   },
   getRatings: (productId, callback) => {
-    Review.find({ product: productId }, (error, doc) => {
+    Review.find({ product: productId }, (error, response) => {
       if (error) {
         callback(error, null);
       } else {
-        callback(null, doc.characteristics.ratings);
+        callback(null, response.characteristics.ratings);
       }
     });
   },
   getCharacteristics: (productId, callback) => {
-    Review.find({ product: productId }, (error, doc) => {
+    Review.find({ product: productId }, (error, response) => {
       if (error) {
         callback(error, null);
       } else {
-        callback(null, doc.characteristics.characteristics);
+        callback(null, response.characteristics.characteristics);
       }
     });
   },
   getRecommended: (productId, callback) => {
-    Review.find({ product: productId }, (error, doc) => {
+    Review.find({ product: productId }, (error, response) => {
       if (error) {
         callback(error, null);
       } else {
-        callback(null, doc.characteristics.recommended);
+        callback(null, response.characteristics.recommended);
+      }
+    });
+  },
+  setReported: (reviewId, callback) => {
+    Review.findOneAndUpdate({ _id: reviewId }, (error, response) => {
+      if (error) {
+        callback(error, null);
+      } else {
+        callback(null, response.report);
+      }
+    });
+  },
+  getReported: (reviewId, callback) => {
+    Review.findOne({ _id: reviewId }, (error, response) => {
+      if (error) {
+        callback(error, null);
+      } else {
+        callback(null, response.report);
+      }
+    });
+  },
+  setHelpfulness: (reviewId, callback) => {
+    Review.findOneAndUpdate({ _id: reviewId }, (error, response) => {
+      if (error) {
+        callback(error, null);
+      } else {
+        callback(null, response.helpfulness);
+      }
+    });
+  },
+  getHelpfulness: (reviewId, callback) => {
+    Review.findOne({ _id: reviewId }, (error, response) => {
+      if (error) {
+        callback(error, null);
+      } else {
+        callback(null, response.helpfulness);
       }
     });
   },
