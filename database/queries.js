4d3d76e37990080219ae50730dc72b35a84e5ea0
Alison Sipos <alison.sipos@icloud.com>
2020-10-26 17:24:42 -0600

    Created initial get all reviews and post one review queries to mySQL database reviews_data.

---
+++ database/queries.js
@@ -0,0 +1,56 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'LoveBud',
+  database: 'reviewData',
+});
+
+connection.connect();
+
+const getReviews = (paramsObj, callback) => {
+  const sql = `SELECT * FROM reviews WHERE product_id=${paramsObj.product_id}`;
+  connection.query(sql, (error, results) => {
+    if (error) {
+      callback(error, null);
+    } else {
+      callback(null, results);
+    }
+  });
+};
+
+// newReviewBody = {
+//   product_id INT NOT NULL,
+//   rating INT NOT NULL,
+//   summary TINYTEXT NOT NULL,
+//   body MEDIUMTEXT NOT NULL,
+//   recommend BOOLEAN NOT NULL,
+//   reviewer_name VARCHAR(50) NOT NULL,
+//   reviewer_email VARCHAR(255) NOT NULL,
+// }
+
+const postReview = (paramsObj, callback) => {
+  const sql = 'INSERT INTO reviews (product_id, rating, summary, body, recommend, reviewer_name, reviewer_email) VALUES (?, ?, ?, ?, ?, ?, ?)';
+  const reviewArgs = [
+    paramsObj.product_id,
+    paramsObj.rating,
+    paramsObj.summary,
+    paramsObj.body,
+    paramsObj.recommend,
+    paramsObj.reviewer_name,
+    paramsObj.reviewer_email,
+  ];
+  connection.query(sql, reviewArgs, (error, results) => {
+    if (error) {
+      callback(error, null);
+    } else {
+      callback(null, results);
+    }
+  });
+};
+
+module.exports = {
+  getReviews,
+  postReview,
+};
